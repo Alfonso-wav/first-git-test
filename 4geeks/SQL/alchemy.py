7e0a21233fa532228deb3b0c74c25f9647aa68f0
Alfonso Massaguer <138866994+Alfonso-wav@users.noreply.github.com>
2023-11-02 19:35:37 +0000

    commitcommit

--- 4geeks/SQL/alchemy.py
+++ 4geeks/SQL/alchemy.py
@@ -16,3 +16,35 @@ class Person(Base):
             "id" : self.id,
             "name" : self.name
         }
+    
+
+# Supongamos que ya tenemos los ingredientes base añadidos con anterioridad
+
+# Ahora insertamos un nuevo ingrediente en la pizza, la carne
+ingredient = Ingredient()
+ingredient.name = "meat"
+ingredient.id = 4
+db.session.add(ingredient)
+
+# Ahora hacemos COMMIT y lo guardamos en la base de datos, de tal forma que fijamos el ingrediente en la Pizza
+db.session.commit()
+
+# Reemplazamos el cuarto ingrediente, que antes era la carne, por los champiñones
+ingredient = Ingredient.query.get(4)
+ingredient.name = mushrooms
+
+# Guardamos un "checkpoint"
+checkpoint_a = db.session.begin_nested()
+
+# Añadimos pepperoni en la pizza
+ingredient = Ingredient()
+ingredient.name = "pepperoni"
+db.session.add(ingredient)
+
+# Un último "checkpoint" antes de añadir el beicon
+checkpoint_b = db.session.begin_nested()
+
+# Insertamos el beicon
+ingredient = Ingredient()
+ingredient.name = "bacon"
+db.session.add(ingredient)
